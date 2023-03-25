21c580de157b25c822852399f3fdbb6d6032cdea
Holivane <holivane@gmail.com>
2023-03-25 17:40:00 -0300

    Testes no put e reordenacao

--- src/test/routes/editoraRoutes.test.js
+++ src/test/routes/editoraRoutes.test.js
@@ -1,88 +1,76 @@
-import { afterEach, describe, expect } from '@jest/globals'
-import app from '../../app.js'
-import request from 'supertest'
+import { afterEach, describe, expect, jest } from "@jest/globals";
+import app from "../../app.js";
+import request from "supertest";
 
-let server
+let server;
 
 beforeEach(() => {
-  const port = 3030
-  server = app.listen(port)
-})
-
+  const port = 3030;
+  server = app.listen(port);
+});
 
 afterEach(() => {
-  server.close()
-})
+  server.close();
+});
 
-describe('GET em /editoras', () => {
-  test('Retorna uma lista de editoras', async () => {
+describe("GET em /editoras", () => {
+  test("Retorna uma lista de editoras", async () => {
     const resposta = await request(app)
-      .get('/editoras')
-      .set('Accept', 'application/json')
-      .expect('content-type', /json/)
-      .expect(200)
+      .get("/editoras")
+      .set("Accept", "application/json")
+      .expect("content-type", /json/)
+      .expect(200);
 
-    expect(resposta.body[0].email).toEqual('e@e.com')
-  })
-})
+    expect(resposta.body[0].email).toEqual("e@e.com");
+  });
+});
 
-let idRespostaPost
-describe('POST em /editoras', () => {
-  test('Adiciona uma nova editora', async () => {
+describe("GET em /editoras/id", () => {
+  test("Retorna o recurso selecionado", async () => {
+    await request(app).get(`/editoras/${idRespostaPost}`).expect(200);
+  });
+});
+
+let idRespostaPost;
+describe("POST em /editoras", () => {
+  test("Adiciona uma nova editora", async () => {
     const resposta = await request(app)
-      .post('/editoras')
+      .post("/editoras")
       .send({
-        nome: 'CDC',
-        cidade: 'Sao Paulo',
-        email: 's@s.com'
+        nome: "CDC",
+        cidade: "Sao Paulo",
+        email: "s@s.com",
       })
-      .expect(201)
+      .expect(201);
 
-      idRespostaPost = resposta.body.content.id
-    })
+    idRespostaPost = resposta.body.content.id;
+  });
 
-    test('Não adiciona se o body estiver vazio', async () => {
-      await request(app)
+  test("Não adiciona se o body estiver vazio", async () => {
+    await request(app).post("/editoras").send({}).expect(400);
+  });
+});
+
+describe("PUT em /editoras/id", () => {
+  test.each([
+    ["nome", { nome: "Casa do codigo" }],
+    ["cidade", { cidade: "SP" }],
+    ["email", { email: "c@c.com" }],
+  ])("Altera o campo %s", async (chave, params) => {
+
+    const requisicao = {request}
+    const spy = jest.spyOn(requisicao, 'request')
+    await requisicao.request(app)
       .put(`/editoras/${idRespostaPost}`)
-      .send({nome: 'Casa do Codigo'})
-      .expect(204)
-    })
-  })
+      .send(params)
+      .expect(204);
 
-  describe('PUT em /editoras/id', () => {
-    test('Altera uma editora', async () => {
-      const resposta = await request(app)
-        .post('/editoras')
-        .send({
-          nome: 'CDC',
-          cidade: 'Sao Paulo',
-          email: 's@s.com'
-        })
-        .expect(201)
+    expect(spy).toHaveBeenCalled()
+  });
+});
 
-        idRespostaPost = resposta.body.content.id
-      })
-
-      test('Não adiciona se o body estiver vazio', async () => {
-        await request(app)
-        .post('/editoras')
-        .send({})
-        .expect(400)
-      })
-    })
-
-  describe('DELETE em /editoras', () => {
-    test('Deleta o recurso adicionado', async () => {
-      await request(app)
-      .delete(`/editoras/${idRespostaPost}`)
-      .expect(200)
-    })
-  })
-
-  describe('GET em /editoras/id', () => {
-    test('Retorna o recurso selecionado', async () => {
-      await request(app)
-      .get(`/editoras/${idRespostaPost}`)
-      .expect(200)
-  })
-})
+describe("DELETE em /editoras/id", () => {
+  test("Deleta o recurso adicionado", async () => {
+    await request(app).delete(`/editoras/${idRespostaPost}`).expect(200);
+  });
+});
