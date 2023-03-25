2f950c8de6edb1fecdff757b340e66107fd6219a
Holivane <holivane@gmail.com>
2023-03-25 16:04:34 -0300

    Teste com mock

--- src/test/models/editora.test.js
+++ src/test/models/editora.test.js
@@ -1,4 +1,4 @@
-import { describe, expect, test } from '@jest/globals'
+import { describe, expect, jest, test } from '@jest/globals'
 import Editora from '../../models/editora.js'
 
 describe('Testa modelo Editora', () => {
@@ -22,12 +22,36 @@ describe('Testa modelo Editora', () => {
     })
   })
 
-  test('Salva uma editora no DB, usando a sintaxe moderna', async () => {
+  test.skip('Salva uma editora no DB, usando a sintaxe moderna', async () => {
     const editora = new Editora(objetoEditora)
 
     const dados = await editora.salvar()
 
     const retornado = await Editora.pegarPeloId(dados.id)
+
+    expect(retornado).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        ...objetoEditora,
+        created_at: expect.any(String),
+        updated_at: expect.any(String)
+      })
+    )
+  })
+
+  test('Faz uma chamada simulada ao DB', () => {
+    const editora = new Editora(objetoEditora)
+
+    editora.salvar = jest.fn().mockReturnValue({
+      id: 10,
+      nome: 'CDC',
+      cidade: 'Sao Paulo',
+      email: 'c@c.com',
+      created_at: '2023-03-25',
+      updated_at: '2023-03-25'
+    })
+
+    const retornado = editora.salvar()
 
     expect(retornado).toEqual(
       expect.objectContaining({
